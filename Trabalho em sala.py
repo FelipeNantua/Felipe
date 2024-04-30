3c637575dfb3650c22ed254daee5594f3f6be986
FelipeNantua <160602880+FelipeNantua@users.noreply.github.com>
2024-04-30 11:01:07 -0300

    Update Trabalho em sala.py

--- Trabalho em sala.py
+++ Trabalho em sala.py
@@ -1,14 +1,1 @@
-def infinito():
-    print("Digite números para adicionar à lista (digite 'done' para terminar):")
-    numeros = []
-    while True:
-        entrada = input()
-        if entrada.lower() == 'done':
-            break
-        else:
-            numeros.append(int(entrada))
-    print("Números coletados:", numeros)
-    
-    
-infinito()    
 
